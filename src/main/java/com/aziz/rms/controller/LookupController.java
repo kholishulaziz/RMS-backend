1351e6cb2e10418beda4c230f4d49c75af3f7da1
kholishulaziz <kholishul.aziz@gmail.com>
2017-04-20 10:39:50 +0700

    create lookup api controller

---
+++ src/main/java/com/aziz/rms/controller/LookupController.java
@@ -0,0 +1,33 @@
+package com.aziz.rms.controller;
+
+import com.aziz.rms.domain.Lookup;
+import com.aziz.rms.repo.LookupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Kholishul_A on 20/04/2017.
+ */
+
+@RestController
+@RequestMapping(path = "/api/lookup")
+public class LookupController {
+
+    @Autowired
+    private LookupRepository lookupRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Iterable<Lookup> findLookupAll(){
+        return lookupRepository.findAll();
+    }
+
+    @RequestMapping(path = "/find/{dataType}", method = RequestMethod.GET)
+    public List<Lookup> findLookupByDataType(@PathVariable String dataType){
+        return lookupRepository.findByDataType(dataType);
+    }
+}
